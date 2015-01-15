c0ce8377d86273ef8a9e24d5fa7eb21cbad7745e
ArielUmansky <umanskyariel@gmail.com>
2015-01-15 01:23:34 -0300

    Terminado seguimiento de usuarios

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -25,8 +25,18 @@ describe "Static pages" do
 					page.should have_selector("li##{item.id}", text: item.content)
 				end
 			end
-		end
 
+			describe "follower/following counts" do
+				let(:other_user) { FactoryGirl.create(:user) }
+				before do
+					other_user.follow!(user)
+					visit root_path
+				end
+				it { should have_link("0 following", href: following_user_path(user)) }
+				it { should have_link("1 follower", href: followers_user_path(user)) }
+			end
+
+		end
 	end	
 	describe "Help page" do
 		before { visit help_path }
@@ -47,4 +57,6 @@ describe "Static pages" do
 		it {should have_selector('h1', text: 'Contact')}
 		it {should have_selector('title', text: full_title('Contact'))}
 	end
+
+
 end
